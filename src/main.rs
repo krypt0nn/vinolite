0c26051e6b470812b71139d30ac593c9a6bd6855
Nikita Podvirnyi <krypt0nn@vk.com>
2025-04-03 17:35:03 +0200

    feat: update help message

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@ use std::path::PathBuf;
 pub mod db_stats;
 pub mod tui;
 
+const HELP: &str = "
+Vinolite  Copyright (C) 2025  Nikita Podvirnyi <krypt0nn@vk.com>
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+
+Analyze SQLite databases space use per table, column and index.
+
+Usage: vinolite <database path>";
+
 fn main() -> anyhow::Result<()> {
     let args = std::env::args().collect::<Vec<String>>();
 
     if args.len() != 2 {
-        eprintln!("Correct command: vinolite <database path>");
+        eprintln!("{HELP}");
 
         return Ok(());
     }
